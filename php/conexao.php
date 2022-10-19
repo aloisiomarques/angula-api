5e68e10610f857a068cbadf0a1787eadccd931ac
Aloisio Marques <aloisio@hotmail.com.br>
2022-10-19 03:34:52 -0300

    build-api

---
+++ php/conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+//variáveis
+
+$url = "localhost";
+$usuario = "me";
+$senha = "Su1S5nHa<>";
+$base = "api";
+
+//Conexão
+
+$conexao = mysqli_connect($url, $usuario, $senha, $base);
---
+++ php/listar.php
@@ -0,0 +1,31 @@
+<?php
+
+//Incluir a conexao
+include("conexao.php");
+
+//sql
+$sql = "SELECT *FROM cursos";
+
+//executar
+$executar = mysqli_query($conexao, $sql);
+
+//vetor
+$cursos = [];
+
+//índice
+$indice = 0;
+
+//laço
+while ($linha = mysqli_fetch_assoc($executar)) {
+  $cursos[$indice]['public_idCurso'] = $linha['public_idCurso'];
+  $cursos[$indice]['public_nomeCurso'] = $linha['public_nomeCurso'];
+  $cursos[$indice]['public_valorCurso'] = $linha['public_valorCurso'];
+
+  $indice++;
+}
+
+//JSON
+json_encode(['cursos' => $cursos]);
+
+var_dump($cursos);
+?>
